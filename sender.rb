29301c796641d9ecb3a905967f8748d612194ca6
Hiromi-nee <kyou.wk@gmail.com>
2012-04-26 10:36:09 +0800

    clean up

--- sender.rb
+++ sender.rb
@@ -1,17 +1,28 @@
+#!/usr/bin/ruby
+# 'end' quits the chat
+# BCS Sender script
+
 require 'socket'
-addr = ['<broadcast>', 33333]
+
 UDPSock = UDPSocket.new
-UDPSock.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
-data = ""
-name = ""
-puts "Input your name: "
-name = gets.chomp
-UDPSock.send(name+" has entered.", 0, addr[0], addr[1])
-while data != 'end' do
-	puts "Input your message: "
-	data = gets.chomp
-	final = name+"> "+data
-	final = name+" has left." if data.eql?("end")
-	UDPSock.send(final, 0, addr[0], addr[1])
+
+def start_sender(addr)
+	
+	UDPSock.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
+	data = ""
+	name = ""
+	puts "Input your name: "
+	name = gets.chomp
+	UDPSock.send(name+" has entered.", 0, addr[0], addr[1])
+	while data != 'end' do
+		puts "Input your message: "
+		data = gets.chomp
+		final = name+"> "+data
+		final = name+" has left." if data.eql?("end")
+		UDPSock.send(final, 0, addr[0], addr[1])
+	end
+	UDPSock.close
 end
-UDPSock.close
+
+addr = ['<broadcast>', 33333]
+start_sender(addr)
